1eb087870bab55fa9d5ef791abeeba0b7d2caf50
Colin Smith <mail.colin.smith@gmail.com>
2013-11-14 21:46:36 -0800

    Added worst score encouragement.

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ var json = 'http://judgingjs.herokuapp.com/leaderboard.json';
 request.get(json, function(err, res) {
     
     console.log(res);
-    console.log('colin waz here');
     
     var teams = JSON.parse(res.body);
     
@@ -15,10 +14,13 @@ request.get(json, function(err, res) {
 
 function calculateMargin(teams) {
         var bestTeam = "1";
+        var worstTeam = "1";
     var ourTeam = "1";
     for (var i = 1; i <= 50; i++) {
         if (teams[i.toString()]["points"] > teams[bestTeam]["points"]) bestTeam = i.toString();
+        if (teams[i.toString()]["points"] < teams[bestTeam]["points"]) worstTeam = i.toString();
         if (teams[i.toString()]["name"] == "Cylon Raider") ourTeam = i.toString();
     }
     console.log("Cylon raiders need only " + (teams[bestTeam]["points"] - teams[ourTeam]["points"]) + " more points to win!");
+    console.log("On the plus side, we're " + (teams[ourTeam]["points"] - teams[worstTeam]["points"]) + " ahead of the worst team!");
 }
